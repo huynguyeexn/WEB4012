e13768e0804783c53a51ab39deff43b1db19563a
huynguyeexn <huynguyeexn@gmail.com>
2021-08-09 14:22:14 +0700

    https fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
