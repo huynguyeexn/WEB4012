f16bdea08146d6bd85f50c38a330d531192d522e
huynguyeexn <huynguyeexn@gmail.com>
2021-08-09 14:01:16 +0700

    fix bug footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -69,7 +69,18 @@
 
 
     <!-- Footer -->
-    <x-footer></x-footer>
+    {{-- <x-footer></x-footer> --}}
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="text-center col-12">
+                    THE NEWS - Made by HUI | Adminto. Coderthemes.com © {{ \Carbon\Carbon::now()->year }}
+                    <p class="p-0 m-0">Nguyễn Ngọc Huy - PS14009 - WD15303</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <!-- End Footer -->
 
 </body>
