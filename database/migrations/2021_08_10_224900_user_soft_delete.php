347f11eb287080eade7dcbd589df03b541d06f5b
huynguyeexn <huynguyeexn@gmail.com>
2021-08-11 12:27:04 +0700

    update user

--- database/migrations/2021_08_10_224900_user_soft_delete.php
+++ database/migrations/2021_08_10_224900_user_soft_delete.php
@@ -13,7 +13,7 @@ class UserSoftDelete extends Migration
      */
     public function up()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
             $table->softDeletes();
         });
@@ -26,7 +26,7 @@ class UserSoftDelete extends Migration
      */
     public function down()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
             $table->dropSoftDeletes();
         });
